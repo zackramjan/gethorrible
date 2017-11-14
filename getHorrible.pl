21cf2a8802825d906fd4c28cfc64577b90c23453
zackramjan <zack.hr@gmail.com>
2017-11-14 03:53:31 +0100

    added initial files

---
+++ getHorrible.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+use XML::Simple;
+use Data::Dumper;
+use File::HomeDir;
+use File::Basename;
+
+my $trackingDir = File::HomeDir->my_home . "/.autoGet";
+mkdir("$trackingDir") unless -e "$trackingDir";
+
+my $toGetFile = $ARGV[0] || die "specify a csv with list of things to for RSS to match and dir to save to (ex: One Piece,one_piece)\n";
+-e $toGetFile  || die "specify a csv with list of things to for RSS to match and dir to save to (ex: One Piece,one_piece)\n";
+
+$ADDCMD = "deluge-console add";
+$DELCMD = "deluge-console pause";
+$DELAGE = 3600;
+
+my %match;
+open(IN,"<$toGetFile");
+while(my $line=<IN>)
+{
+	chomp $line;
+	next unless $line =~ /,/;
+	my @a = split(/,/,$line);
+	$match{$a[0]}=$a[1];
+}
+
+
+my $xml = `wget -O - http://horriblesubs.info/rss.php?res=sd` ;
+my $ref = XMLin($xml, ForceArray => 0, KeyAttr => "title");
+$torrents = $ref->{"channel"}->{item};
+
+for my $r (keys %{$torrents})
+{
+	for my $m (keys %match)
+	{
+		if($r =~ /$m/i && ! -e "$match{$m}/$r")
+		{
+			print "$r matched regex $m\n\tGrabbing magnet to dir $match{$m} \n";
+			runcmd("$ADDCMD -p $match{$m} \"$torrents->{$r}->{link}\"");
+			runcmd("touch \"$trackingDir/$r\"");
+		}
+	}
+}
+
+for my $f (glob ("$trackingDir/*"))
+{
+	runcmd("$DELCMD \\\"" . basename($f) . "\\\"") if (time - ( stat($f))[9]) > 3600;
+}
+
+sub runcmd{
+        my $cmd=shift @_;
+        my $caller=(caller(1))[3];
+        print STDERR "$caller\t$cmd\n";
+        system($cmd);
+}
